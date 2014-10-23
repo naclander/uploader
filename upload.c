a9bd1ae797ff1f1de122751f28ad51fe1e04f9ce
Nathan Aclander <naclander@linux.ucla.edu>
2014-10-23 00:56:00 -0700

    Added text_list_to_html function and helpers

--- upload.c
+++ upload.c
@@ -68,6 +68,60 @@ bool file_exists(char * file_path){
 	return(true);
 }
 
+char * free_old_return_new(char * old, char * new){
+	free(old);
+	return(new);
+}
+
+
+//Parses the text file TEXT_FILE for strings, creates a new formatted string,
+//and places the new formatted string in the first %s of current_html_file. It
+//wil also set input_file_size to the size of the newly expanded html file.
+//current_html_file will be freed
+//Input:
+//current_html_file: the html file that will have the new formatted text_list string
+//placed into it. It must contain two "%s".
+//input_file_size: This value will be set to the newly expanded size of the html
+//file
+//Returns:
+//a pointer to the expanded html file. This pointer must be freed. current_html_file
+//is freed by this function
+char * text_list_to_html(char * current_html_file, unsigned int * input_file_size){
+	char * text_list = read_file(TEXT_FILE, input_file_size);
+	char * new_html_file = NULL;
+
+	if(text_list == NULL){
+		//TODO possibly make this into its own function because its the
+		//same 3 lines at the end of this function
+		Sasprintf(new_html_file,current_html_file,"","%s");
+		*input_file_size = strlen(new_html_file);
+		return(free_old_return_new(current_html_file,new_html_file));
+	}
+
+	char * formatted_text_list = NULL;
+	Sasprintf(formatted_text_list,"<ul style=\"list-style-type:disc\">");
+	char *tok = text_list;
+	while((tok = strtok(tok, "\n")) != NULL){
+		Sasprintf(formatted_text_list, "%s\n <li> %s </li>",formatted_text_list,tok);
+        tok = NULL;
+	}
+	Sasprintf(formatted_text_list,"%s\n</ul>",formatted_text_list);
+
+	Sasprintf(new_html_file,current_html_file,formatted_text_list,"%s");
+	*input_file_size = strlen(new_html_file);
+	return(free_old_return_new(current_html_file,new_html_file));
+}
+
+char * file_list_to_html(unsigned int * input_file_size){
+	*input_file_size = 10;
+	return malloc(10*sizeof(char));
+}
+
+void write_and_free(onion_response * res, char * file, unsigned int input_size){
+	onion_response_write(res, file, input_size);
+	free(file);
+}
+
 onion_connection_status main_page(void *_, onion_request *req, onion_response *res){
 	unsigned int input_file_size;
 	char * html_file = read_html_file("index.html", &input_file_size);
