cce90fdc4a2123d1f28d280d950e2c6d383d0f94
Nathan Aclander <naclander@linux.ucla.edu>
2014-11-07 15:01:42 -0800

    Refractor page response

--- upload.c
+++ upload.c
@@ -111,68 +111,75 @@ char * text_list_to_html(){
 }
 
 
+void handle_file_query(onion_request * req, onion_response * res){
+	/* TODO: Find a way to not allocate a string for this */
+	char * dir_path = NULL;
+	char * buffer = NULL;
+	FILE * fp = NULL;
+
+	Sasprintf(dir_path,"%s%s",FILE_DIRECTORY,onion_request_get_query(req,"1"));
+	if((fp = fopen(dir_path,"rb")) == NULL){
+		onion_shortcut_response("<b> 404 File Not Found </b>", 404, req, res);
+		return;
+	}
+	free(dir_path);
+	fseek(fp, 0, SEEK_END);
+	int size = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+	buffer = malloc(size);
+	/*TODO: Will break on files of size 0*/
+	if (fread(buffer, 1, size, fp) == size && size != 0){
+		onion_response_set_header(res, "Content-Type", onion_mime_get(dir_path));
+		onion_response_write(res, buffer, size);
+	}
+	fclose(fp);
+	free(buffer);
+}
+
+void handle_main_page(onion_request * req, onion_response * res){
+	char * html_file = read_file(HTML_PAGE);
+	char * html_text_list = text_list_to_html();
+	char * html_file_list = file_list_to_html();
+
+	Sasprintf(html_file,html_file,html_text_list,html_file_list);
+
+	onion_response_write(res, html_file, strlen(html_file));
+
+	free(html_file);
+	free(html_text_list);
+	free(html_file_list);
+}
+
 onion_connection_status main_page(void *_, onion_request *req, onion_response *res){
 
-	if (onion_request_get_query(req, "file")){
+	onion_request_get_query(req,"file") ? handle_file_query(req,res) :
+										  handle_main_page(req,res);
+	return(OCS_PROCESSED);
+}
+
+onion_connection_status post_data(void *_, onion_request *req, onion_response *res){
+	//user_data could be null if "text" form was not filled
+	const char *user_data=onion_request_get_post(req,"text");
+	if(strcmp(user_data,"")){
+		//onion_response_printf(res, "The user wrote: %s", user_data);
+		/* TODO: Fix cross site scripting vulnerability */
 		/* TODO: Find a way to not allocate a string for this */
-		char * dir_path = NULL;
-		char * buffer = NULL;
-		FILE * fp = NULL;
-
-		Sasprintf(dir_path,"%s%s",FILE_DIRECTORY,onion_request_get_query(req,"1"));
-		if((fp = fopen(dir_path,"rb")) == NULL){
-			return(onion_shortcut_response("<b> 404 File Not Found </b>", 404, req, res));
+		char * command = NULL;
+		FILE * input_stream = fopen(TEXT_FILE,"a");
+		if(fprintf(input_stream,"%s\n",user_data) < 0){
+			printf("Couldn't append to file\n");
+			exit(1);
 		}
-		free(dir_path);
-		fseek(fp, 0, SEEK_END);
-		int size = ftell(fp);
-		fseek(fp, 0, SEEK_SET);
-		buffer = malloc(size);
-		/*Will break on files of size 0*/
-		if (fread(buffer, 1, size, fp) == size && size != 0){
-			onion_response_set_header(res, "Content-Type", onion_mime_get(dir_path));
-			onion_response_write(res, buffer, size);
-		}
-		fclose(fp);
-		free(buffer);
+		fclose(input_stream);
+		free(command);
 	}
-	else{
-		char * html_file = read_file(HTML_PAGE);
-		char * html_text_list = text_list_to_html();
-		char * html_file_list = file_list_to_html();
-
-		Sasprintf(html_file,html_file,html_text_list,html_file_list);
-
-		onion_response_write(res, html_file, strlen(html_file));
-
-		free(html_file);
-		free(html_text_list);
-		free(html_file_list);
-	}
-	return OCS_PROCESSED;
+	return(main_page(NULL, req,res));
 }
 
 int main(int argc, char **argv){
 	char * port = "8080";
 	onion * o=onion_new(O_ONE_LOOP);
 	onion_url * urls=onion_root_url(o);
-	
-	/*
-	onion_url_add_static(urls, "",
-"<html>\n"
-"<head>\n"
-" <title>Simple post example</title>\n"
-"</head>\n"
-"\n"
-"Write something: \n"
-"<form method=\"POST\" action=\"data\">\n"
-"<input type=\"text\" name=\"text\">\n"
-"<input type=\"submit\">\n"
-"</form>\n"
-"\n"
-"</html>\n", HTTP_OK);
-	*/
-
 	onion_url_add(urls, "data", post_data);
 	onion_url_add(urls, "", main_page);
 	onion_url_add(urls, "^(.*)$", main_page);
